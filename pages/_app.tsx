af89beb8d48a674d8a36ccb25cfc533bfac1cd5f
Giorgi Tk <gtkesh@gmail.com>
2021-03-25 22:07:13 +0400

    Add dark mode support on homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import "../styles/global.css";
 import { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Toggle from "react-toggle";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
