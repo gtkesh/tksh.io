3aa801267996c86ffeb2f44b3230c02f3b29301f
Giorgi Tk <gtkesh@gmail.com>
2021-03-29 15:27:21 +0400

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import "../styles/global.css";
 import { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/dist/client/router";
 import { ThemeProvider } from "next-themes";
+import { pageview } from "../lib/gtag";
 
 function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider defaultTheme="light">
       <Component {...pageProps} />;
